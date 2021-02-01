833b32c83283196eec5c331e0daf784f9acc086d
Gilles Barbier <gilles@zenaton.com>
2021-02-01 21:47:01 +0100

    update client with new async syntax

--- src/main/java/hello/world/Client.java
+++ src/main/java/hello/world/Client.java
@@ -1,5 +1,6 @@
 package hello.world;
 
+import hello.world.tasks.HelloWorldService;
 import hello.world.workflows.HelloWorld;
 import io.infinitic.pulsar.InfiniticClient;
 import javax.annotation.Nullable;
@@ -8,10 +9,11 @@ public class Client {
     public static void main(String[] args) {
         InfiniticClient client = InfiniticClient.fromFile("infinitic.yml");
         @Nullable String name = args.length>0 ? args[0] : null;
-        client.startWorkflowAsync(
-                HelloWorld.class,
-                w -> w.greet(name)
-        ).join();
+
+        // create a stub from HelloWorld interface
+        HelloWorld helloWorld = client.workflow(HelloWorld.class);
+        // dispatch a workflow
+        client.async(helloWorld, w -> w.greet(name));
 
         client.close();
     }
